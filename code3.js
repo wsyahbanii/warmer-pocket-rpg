24c8c850862dce21b2c2e8079a169e6b8c045891
Wahyu Syahbani <wahyuusyahbanii@gmail.com>
2026-07-17 21:30:38 +0700

    add some goodd

--- code3.js
+++ code3.js
@@ -93,7 +93,7 @@ gdjs.MenuKarakterCode.GDDandiObjects1= [];
 gdjs.MenuKarakterCode.GDDandiObjects2= [];
 
 
-gdjs.MenuKarakterCode.userFunc0xbc42b0 = function GDJSInlineCode(runtimeScene) {
+gdjs.MenuKarakterCode.userFunc0x965c78 = function GDJSInlineCode(runtimeScene) {
 "use strict";
 // Cegah kode disuntikkan berkali-kali
 if (!window.gtagTelahMasuk) {
@@ -159,7 +159,7 @@ gdjs.MenuKarakterCode.eventsList0 = function(runtimeScene) {
 {
 
 
-gdjs.MenuKarakterCode.userFunc0xbc42b0(runtimeScene);
+gdjs.MenuKarakterCode.userFunc0x965c78(runtimeScene);
 
 }
 
